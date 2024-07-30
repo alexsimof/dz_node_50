b7266dec277b7c417919fda7e1a6dcc50b0d46fd
Alex Simonov <alex.simonof@gmail.com>
2024-07-30 16:28:43 +0300

    made function to convert byte to gigabyte

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-import './os-module.js'
+
+import { writeFile } from "fs";
+import { sysInfo } from "./os-module.js"
+
+writeFile('mySys.txt', sysInfo, (err) => {
+if (err){
+  console.error("Ошибка!");
+  return;
+}
+console.log("Файл успешно записан!");
+})
--- os-module.js
+++ os-module.js
@@ -1,5 +1,5 @@
 import { platform, arch, freemem, totalmem, cpus } from 'os';
-import { writeFile } from "fs";
+
 
 const system = platform();
 const architect = arch();
@@ -8,19 +8,18 @@ const totalMemory = totalmem();
 const {model} = cpus()[0];
 const numberCores = cpus().length;
 
-const sysInfo = `
+function convertByteToGigaByte(numb) {
+  let numbGb = (numb/(1024 ** 3)).toFixed(2)
+  return numbGb
+}
+
+export const sysInfo = `
   Platform: ${system}
   Arch: ${architect}
-  Free: ${(freeMemory/(1024 ** 3)).toFixed(2)}Гб.
-  Total: ${(totalMemory/(1024 ** 3)).toFixed(2)}Гб.
+  Free: ${convertByteToGigaByte(freeMemory)}Гб.
+  Total: ${convertByteToGigaByte(totalMemory)}Гб.
   Cpu: ${model}
   Cores: ${numberCores}
   `
-
-writeFile('mySys.txt', sysInfo, (err) => {
-if (err){
-  console.error("Ошибка!");
-  return;
-}
-console.log("Файл успешно записан!");
-})
+// (freeMemory/(1024 ** 3)).toFixed(2)
+// (totalMemory/(1024 ** 3)).toFixed(2)
